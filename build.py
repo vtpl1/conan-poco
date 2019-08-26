be1e8eef91d1f079ae23eb24c19607d40875825d
vtpl1 <monotosh.das@videonetics.com>
2019-08-26 17:23:06 +0530

    CONAN_REMOTES moved to travis.yml

--- build.py
+++ build.py
@@ -5,7 +5,6 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    upload_remote = "https://api.bintray.com/conan/bincrafters/public-conan"
-    builder = ConanMultiPackager(remotes=upload_remote)
+    builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="Poco:shared", pure_c=False)
     builder.run()
