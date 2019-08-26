d3703511c45587b9e964180165439296d688842b
vtpl1 <monotosh.das@videonetics.com>
2019-08-26 15:08:21 +0530

    Making changes for CI integration

--- build.py
+++ build.py
@@ -6,19 +6,5 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    if platform.system() == "Windows":
-        builder.add(settings={"arch": "x86", "build_type": "Debug", "compiler": "Visual Studio", "compiler.version": 15, "compiler.runtime": "MTd"},
-                    options={}, env_vars={}, build_requires={})
-        builder.add(settings={"arch": "x86_64", "build_type": "Debug", "compiler": "Visual Studio", "compiler.version": 15, "compiler.runtime": "MTd"},
-                    options={}, env_vars={}, build_requires={})
-        builder.add(settings={"arch": "x86", "build_type": "Release", "compiler": "Visual Studio", "compiler.version": 15, "compiler.runtime": "MT"},
-                    options={}, env_vars={}, build_requires={})
-        builder.add(settings={"arch": "x86_64", "build_type": "Release", "compiler": "Visual Studio", "compiler.version": 15, "compiler.runtime": "MT"},
-                    options={}, env_vars={}, build_requires={})
-    else:
-        builder.add(settings={"arch": "x86", "build_type": "Debug"},
-                    options={}, env_vars={}, build_requires={})
-        builder.add(settings={"arch": "x86_64", "build_type": "Debug"},
-                    options={}, env_vars={}, build_requires={})
-    #builder.add_common_builds(shared_option_name="Poco:shared", pure_c=False)
+    builder.add_common_builds(shared_option_name="Poco:shared", pure_c=False)
     builder.run()
