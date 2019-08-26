9d9b1a936fa4c1a7e8b3dc24c2114b59f43b621d
vtpl1 <monotosh.das@videonetics.com>
2019-08-26 16:20:24 +0530

    Update build.py

--- build.py
+++ build.py
@@ -5,6 +5,6 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(remotes=CONAN_UPLOAD)
     builder.add_common_builds(shared_option_name="Poco:shared", pure_c=False)
     builder.run()
